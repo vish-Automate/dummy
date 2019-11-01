81c9c19ae20c12ae33678276a9f0789c955b7773
vishwas <vishwas@DESKTOP-36JED47>
2019-11-01 15:53:39 +0530

    design

---
+++ src/dummy123/Hello.java
@@ -0,0 +1,10 @@
+package dummy123;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+           System.out.println("hello pradeep anna");
+	}
+
+}
